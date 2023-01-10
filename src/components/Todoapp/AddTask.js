e16bf144d1bc91de00d28f77a285770a4b301173
Gopika V R <gopikavr1712@gmail.com>
2023-01-10 14:53:44 +0530

    first commit

---
+++ src/components/Todoapp/AddTask.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+const AddTask = ({addTask}) => {
+    const[value,setValue]=useState("");
+    const addItem =()=>{
+        addTask(value);
+        setValue("")
+    }
+  return (
+    <>
+        <div className='input-container'>
+            <input type="text" className='input'
+            placeholder='Add task here...'
+            value={value}
+            onChange={(e)=>{setValue(e.target.value)}}
+            />
+            <button onClick={addItem} className='add-btn'>ADD</button>
+        </div>
+    </>
+  )
+}
+
+export default AddTask
